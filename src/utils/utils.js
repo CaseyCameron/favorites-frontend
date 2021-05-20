949a6b3efd0e54a64a8f548bbc464e871d1adebe
tif-calin <culitif@tuta.io>
2021-05-20 11:05:47 -0700

    set up auth endpoint interaction

---
+++ src/utils/utils.js
@@ -0,0 +1,25 @@
+import request from 'superagent';
+
+const URL = 'https://boiling-wildwood-01634.herokuapp.com';
+
+export async function signUp(user) {
+  const response = await request 
+    .post(URL + '/api/auth/signup') 
+    .ok(res => res.status < 500) 
+    .send(user);
+  
+  if (response.status === 400) throw response.body; 
+
+  return response.body;
+}
+
+export async function logIn(user) {
+  const response = await request 
+    .post(URL + '/api/auth/signin') 
+    .ok(res => res.status < 500) 
+    .send(user);
+  
+  if (response.status === 400) throw response.body; 
+
+  return response.body;
+}
